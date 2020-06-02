9a6be5f5812b270d6bd863686bf993a1db22bc54
NareshReddyGarivi <naresh.garivi@outlook.com>
2020-06-02 11:20:53 +0530

    Added tests and service layer

---
+++ SpellNumber.Services/ISpellNumber.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpellNumber.Services
+{
+    public interface ISpellNumber
+    {
+    }
+}
---
+++ SpellNumber.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpellNumber.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
